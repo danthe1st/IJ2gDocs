da6e202c92ce14ebaab56399b3a3da3c129849e1
danthe1st <daniel@wwwmaster.at>
2021-08-15 15:22:28 +0200

    use Java 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,14 +51,11 @@ changelog {
 }
 
 tasks {
-    // Set the compatibility versions to 1.8
+    // Set the compatibility versions to 11
     withType<JavaCompile> {
-        sourceCompatibility = "1.8"
-        targetCompatibility = "1.8"
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
     }
-    //withType<KotlinCompile> {
-    //    kotlinOptions.jvmTarget = "1.8"
-    //}
 
     //withType<Detekt> {
     //    jvmTarget = "1.8"
